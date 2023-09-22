93690ed1fdea4d103325ff32d281cd96d57e05c8
Akash santhosh <akash777kk@gmail.com>
2023-09-22 23:24:30 +0530

    Add project files.

---
+++ FactoryAbstractConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using FactoryAbstractClassLibrary.Models;
+
+namespace FactoryAbstractConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICarFactory sUVFactory = new SUVFactory();
+            ITesla TeslaSuv = sUVFactory.CreateTesla();
+            TeslaSuv.start();
+            TeslaSuv.stop();
+            Console.WriteLine("===========================================");
+            IBmw bmwSuv=sUVFactory.CreateBMW();
+            bmwSuv.start();
+            bmwSuv.stop();
+            Console.WriteLine("===========================================");
+
+            ICarFactory SedanFactory = new SedanFactory();
+            IBmw Bmwsedan = SedanFactory.CreateBMW();
+            Bmwsedan.start();
+            Bmwsedan.stop();
+             Console.WriteLine("===========================================");
+            ITesla TeslaSedan=SedanFactory.CreateTesla();
+            TeslaSedan.start();
+            TeslaSedan.stop();
+            Console.WriteLine("===========================================");
+        }
+    }
+}
